c3818f244544c30dbdcdf3db72f4dc4f8f374082
Adam Gray <a.gray.4797@gmail.com>
2021-09-16 13:53:21 +0100

    added cockroach hs sol

---
+++ Beginner Series #4 Cockroach/cockroach.hs
@@ -0,0 +1,7 @@
+-- Kata 55fab1ffda3e2e44f00000c6: Beginner Series #4 Cockroach
+-- By: Vortus
+-- OBJECTIVE: Complete the function to convert the given
+-- cockroach speed from km/h to cm/s
+
+cockroachSpeed :: Double -> Integer
+cockroachSpeed s = floor ((1000 * s) / 36)
