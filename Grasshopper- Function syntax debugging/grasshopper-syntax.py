47ecf58a3c0d7a30d9f158b0070a6c0697e0cc78
Adam Gray <a.gray.4797@gmail.com>
2021-09-16 15:06:57 +0100

    added grasshopper function syntax

---
+++ Grasshopper- Function syntax debugging/grasshopper-syntax.py
@@ -0,0 +1,10 @@
+'''
+Kata 56dae9dc54c0acd29d00109a: Grasshopper- Function syntax debugging
+By: danleavitt0
+OBJECTIVE: Fix the syntax mistakes in the following function definition
+def main [verb, noun]
+return verb + noun
+'''
+
+def main(verb, noun):
+    return verb + noun
