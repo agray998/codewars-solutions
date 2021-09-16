1c12747da3d5fc5fa7ff1acb32704b6041c9002c
Adam Gray <a.gray.4797@gmail.com>
2021-09-16 14:55:52 +0100

    added variable assignment hs sol

---
+++ Basic variable assignment/var-assignment.hs
@@ -0,0 +1,7 @@
+-- Kata 50ee6b0bdeab583673000025: Basic variable assignment
+-- By: Ira
+-- OBJECTIVE: Fix the given code to create a variable 'name' with the value "codewa.rs"
+
+a = "code"
+b = "wa.rs"
+name = a ++ b
