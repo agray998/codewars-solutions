73c949d9958f848a06d4976b4563703ce63c41aa
Adam Gray <a.gray.4797@gmail.com>
2021-09-16 13:36:50 +0100

    added opposites attract Haskell sol

---
+++ Opposites Attract/opposites-attract.hs
@@ -0,0 +1,9 @@
+-- Kata 555086d53eac039a2a000083: Opposites Attract
+-- By: jbarget
+-- OBJECTIVE: To complete the function to return true
+-- if one flower has an odd number of petals and the 
+-- other an even number, and false otherwise
+
+inlove :: Int -> Int -> Bool
+inlove a b | mod (a + b) 2 == 1 = True
+           | otherwise = False
