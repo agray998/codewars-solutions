fa7683f4d255d7fbc0e56173a8433818ba653206
Adam Gray <a.gray.4797@gmail.com>
2021-09-16 13:42:07 +0100

    added multiply hs sol

---
+++ Multiply/multiply.hs
@@ -0,0 +1,9 @@
+-- Kata 50654ddff44f800200000004: Multiply
+-- By: 
+-- OBJECTIVE: To fix the following code:
+-- multiply :: Int -> Int -> Int
+-- multiply a b = do
+--   return $ a * b
+
+multiply :: Int -> Int -> Int
+multiply = (*)
