67eb5b8989dc9b928a2f8adcad9b61b485c03143
Adam Gray <a.gray.4797@gmail.com>
2021-09-17 16:32:39 +0100

    added find smallest integer

---
+++ Find the smallest integer in the array/smallest-int.hs
@@ -0,0 +1,6 @@
+-- Kata 55a2d7ebe362935a210000b2: Find the smallest integer in the array
+-- By: dukeofgarda
+-- OBJECTIVE: Complete the function to return the smallest integer from a given array
+
+findSmallestInteger :: [Int] -> Int
+findSmallestInteger = minimum
---
+++ Find the smallest integer in the array/smallest-int.py
@@ -0,0 +1,8 @@
+'''
+Kata 55a2d7ebe362935a210000b2: Find the smallest integer in the array
+By: dukeofgarda
+OBJECTIVE: Complete the function to return the smallest integer from a given array
+'''
+
+def find_smallest_int(arr):
+    return min(arr)
