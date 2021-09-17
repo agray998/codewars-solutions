fa5ffe0aeb226898f2f874c309cd95a9a9d393f6
Adam Gray <a.gray.4797@gmail.com>
2021-09-17 16:27:22 +0100

    added messi goals

---
+++ Grasshopper- Messi Goals/messi-goals.py
@@ -0,0 +1,17 @@
+'''
+Kata 55ca77fa094a2af31f00002a: Grasshopper- Messi Goals
+By: danleavitt0
+OBJECTIVE: Given the following information: 
+Competition         Goals
+-----------------|--------
+La Liga	         |   43
+Champions League |	 10
+Copa del Rey	 |   5
+Use variables to find the total goals scored by Messi across the three competitions
+'''
+
+la_liga_goals = 43
+champions_league_goals = 10
+copa_del_rey_goals = 5
+
+total_goals = la_liga_goals + champions_league_goals + copa_del_rey_goals
