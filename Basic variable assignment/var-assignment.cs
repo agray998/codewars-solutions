acfb31597942065423860330803eec1be1970786
Adam Gray <a.gray.4797@gmail.com>
2021-09-18 20:47:49 +0100

    added variable assignment C#

---
+++ Basic variable assignment/var-assignment.cs
@@ -0,0 +1,14 @@
+/*
+Kata 50ee6b0bdeab583673000025: Basic variable assignment
+By: Ira
+OBJECTIVE: Fix the given code to create a variable 'name' with the value "codewa.rs"
+*/
+
+using System;
+
+public class Kata
+{
+  public static String A = "code";
+  public static String B = "wa.rs";
+  public static String Name = A + B;
+}
