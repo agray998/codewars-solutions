b011b8e35d9317627c246829d776eb3c9ecfd262
Adam Gray <a.gray.4797@gmail.com>
2021-09-18 18:40:26 +0100

    added opposites attract C#

---
+++ Opposites Attract/opposites-attract.cs
@@ -0,0 +1,21 @@
+/*
+Kata 555086d53eac039a2a000083: Opposites Attract
+By: jbarget
+OBJECTIVE: To complete the function to return true
+if one flower has an odd number of petals and the 
+other an even number, and false otherwise
+*/
+
+using System;
+
+public class LoveDetector
+{
+    public static bool lovefunc(int flower1, int flower2)
+    { 
+        if ((flower1 + flower2) % 2 == 1)
+        {
+          return true;
+        }
+        return false;
+    }
+}
