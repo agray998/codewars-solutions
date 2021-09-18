3bef93a0868a6e1ac1ebabb9831f7d56964fc3d3
Adam Gray <a.gray.4797@gmail.com>
2021-09-18 18:43:11 +0100

    refactored opposites attract C#

--- Opposites Attract/opposites-attract.cs
+++ Opposites Attract/opposites-attract.cs
@@ -12,10 +12,6 @@ public class LoveDetector
 {
     public static bool lovefunc(int flower1, int flower2)
     { 
-        if ((flower1 + flower2) % 2 == 1)
-        {
-          return true;
-        }
-        return false;
+        return (flower1 + flower2) % 2 == 1;
     }
 }
