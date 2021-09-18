8e8a34107d6bf6c66c135fe3167c8e20fd319931
Adam Gray <a.gray.4797@gmail.com>
2021-09-18 16:36:52 +0100

    added filter even numbers

---
+++ Filtering even numbers (Bug Fixes)/filter-evens.py
@@ -0,0 +1,9 @@
+'''
+Kata 566dc566f6ea9a14b500007b: Filtering even numbers (Bug Fixes)
+By: aryan-firouzian
+OBJECTIVE: Fix the provided code so that it returns a list 
+containing only the odd elements of a given list
+'''
+
+def kata_13_december(lst): 
+    return [i for i in lst if i % 2]
