b38a20ce165978a85db95e65deb0ef1dcd6d5fde
Adam Gray <a.gray.4797@gmail.com>
2021-09-18 16:57:12 +0100

    added filter even numbers c#

---
+++ Filtering even numbers (Bug Fixes)/filter-evens.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+    {
+        public static List<int> FilterOddNumber(List<int> listOfNumbers)
+        {
+            for (int i = 0; i < listOfNumbers.Count; i++)
+            {
+                if (listOfNumbers[i] % 2 == 0)
+                {
+                    listOfNumbers.RemoveAt(i);
+                    i--;
+                }
+            }
+            return listOfNumbers;
+        }
+    }
